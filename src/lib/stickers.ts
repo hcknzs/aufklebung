4e49943acaaca224500020e05e4905d8a1790e5b
Moritz Jacobs <mail@moritzjacobs.de>
2024-07-20 11:33:10 +0200

    wip

--- src/lib/stickers.ts
+++ src/lib/stickers.ts
@@ -1,17 +1,69 @@
-import { z } from "zod";
-import stickersJson from "./stickers.json";
+export type Parameters = {
+	backgroundColor?: string;
+	foregroundColor?: string;
+	text1?: string;
+	text2?: string;
+	url?: string;
+};
 
-const stickerSchema = z.object({
-	slug: z.string().min(1),
-	name: z.string().min(1),
-	thumbnailSrc: z.string().min(1),
-});
+type Sticker = {
+	shape: "square" | "circle";
+	slug: string;
+	name: string;
+	thumbnailSrc: string;
+	parameters: {
+		backgroundColor: boolean;
+		foregroundColor: boolean;
+		text1: boolean;
+		text2: boolean;
+		url: boolean;
+	};
+};
 
-export type Sticker = z.infer<typeof stickerSchema>;
-
-const fileSchema = z.array(stickerSchema);
-
-export const stickers = fileSchema.parse(stickersJson);
+export const stickers = [
+	{
+		slug: "hasst-die-afd",
+		shape: "circle",
+		name: "Ganz … hasst die AfD!",
+		thumbnailSrc:
+			"https://media2.giphy.com/media/ZJaXOsbM012mRodFiQ/giphy.gif?cid=47028fa8sx12d824xrvy8zo8bq3w00f23hoj11gqqzus7q0f&ep=v1_gifs&rid=giphy.gif&ct=g",
+		parameters: {
+			text1: true,
+			text2: false,
+			backgroundColor: true,
+			foregroundColor: true,
+			url: false,
+		},
+	},
+	{
+		slug: "uhm-actually",
+		shape: "square",
+		name: "Uhm, actually...",
+		thumbnailSrc:
+			"https://media2.giphy.com/media/ZJaXOsbM012mRodFiQ/giphy.gif?cid=47028fa8sx12d824xrvy8zo8bq3w00f23hoj11gqqzus7q0f&ep=v1_gifs&rid=giphy.gif&ct=g",
+		parameters: {
+			text1: false,
+			text2: false,
+			backgroundColor: false,
+			foregroundColor: false,
+			url: true,
+		},
+	},
+	{
+		slug: "-innen",
+		shape: "square",
+		name: "*INNEN",
+		thumbnailSrc:
+			"https://media3.giphy.com/media/Y01jP8QeLOox2/giphy.gif?cid=47028fa8sx12d824xrvy8zo8bq3w00f23hoj11gqqzus7q0f&ep=v1_gifs&rid=giphy.gif&ct=g",
+		parameters: {
+			text1: false,
+			text2: false,
+			backgroundColor: true,
+			foregroundColor: true,
+			url: false,
+		},
+	},
+] satisfies Array<Sticker>;
 
 export const getStickerInfoBySlug = (slug: string) => {
 	return stickers.find((sticker) => sticker.slug === slug);
