bee102ee3f7e90dd2f92f35be7fe05ff34eb875a
Lukas Frey <lukas.frey03@outlook.de>
2024-07-21 12:31:11 +0200

    replace emoji with sparkles svg

--- src/components/color-randomizer.tsx
+++ src/components/color-randomizer.tsx
@@ -25,7 +25,19 @@ export const ColorRandomizer = ({
 
 	return (
 		<Button onClick={handleClick} className="emoji-white text-xl">
-			✨<span className="sr-only">Farben zufällig</span>
+			<svg
+				className="text-accent h-5 w-5"
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 14 14"
+			>
+				<path
+					d="M4.4,8.95c3.17,0,4.55-1.33,4.55-4.55,0,3.22,1.37,4.55,4.55,4.55-3.18,0-4.55,1.37-4.55,4.55,0-3.18-1.38-4.55-4.55-4.55ZM.5,3.42c2.04,0,2.92-.86,2.92-2.92,0,2.07.88,2.92,2.92,2.92-2.04,0-2.92.88-2.92,2.92,0-2.04-.89-2.92-2.92-2.92Z"
+					fill="currentColor"
+					stroke="currentColor"
+					stroke-linejoin="round"
+				/>
+			</svg>
+			<span className="sr-only">Farben zufällig</span>
 		</Button>
 	);
 };
