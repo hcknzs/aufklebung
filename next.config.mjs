11e2b025a72883b2e173af80a546d40fc83cb28a
Moritz Jacobs <mail@moritzjacobs.de>
2024-07-21 11:40:57 +0200

    Un-ALF

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		domains: ["media2.giphy.com", "media3.giphy.com"],
-	},
 	logging: {
 		fetches: {
 			fullUrl: process.env.LOCALHOST === "true",
